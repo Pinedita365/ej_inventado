dc5eaf7cfb6dbf6f80dffbce48df5f0683c6a8d6
Pinedita365 <javiercoco2201@gmail.com>
2026-02-02 22:37:21 +0100

    aaaa

--- src/main/java/com/example/ej_inventado/config/WebConfig.java
+++ src/main/java/com/example/ej_inventado/config/WebConfig.java
@@ -1,29 +1,28 @@
-package com.example.ej_inventado.config;
+import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(new Locale("es"));
+        CookieLocaleResolver slr = new CookieLocaleResolver();
+        slr.setDefaultLocale(new Locale("es")); // Fuerza español por defecto
+        slr.setCookieName("language");
         return slr;
     }
 
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
         LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang");
+        lci.setParamName("lang"); // El botón debe enviar ?lang=es o ?lang=en
         return lci;
     }
 
